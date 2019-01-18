2fe910fcc3b58df61c6b8b9059e23d35e504f6b7
Jenny Chang <jennychang@Jennys-MacBook-Pro.local>
2019-01-18 14:41:49 -0800

    FINAL

---
+++ TipCalculator/TextFieldViewController.h
@@ -0,0 +1,17 @@
+//
+//  TextFieldViewController.h
+//  TipCalculator
+//
+//  Created by Jenny Chang on 18/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextFieldViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TipCalculator/TextFieldViewController.m
@@ -0,0 +1,32 @@
+//
+//  TextFieldViewController.m
+//  TipCalculator
+//
+//  Created by Jenny Chang on 18/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import "TextFieldViewController.h"
+
+@interface TextFieldViewController ()
+
+@end
+
+@implementation TextFieldViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
